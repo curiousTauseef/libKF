5c777c77ecee2f3efac6c56c6ac3d9721142a79d
kirk <kirk@PossomServer.hsd1.mi.comcast.net>
2016-02-01 17:39:30 -0500

    Working on filter convergence

--- tests/src/filter.c
+++ tests/src/filter.c
@@ -3,17 +3,30 @@
 
 int test(void)
 {
-	kf_t filter = {};
+	kf_t f = {};
 
-	assert(!kfCreateFilter(&filter, 1));
+	assert(!kfCreateFilter(&f, 1));
 	Log("Filter structures allocated", 1);		
 
+	f.matStateTrans[0][0] = 1;
+	f.matTrans[0][0]      = 1;
+	f.matCtrlInput[0][0]  = 0; // no input controls
+
+	// start with 100% uncertainty
+	f.epoch[0].matVarCovar[0][0] = 1;	
+	f.epoch[1].matVarCovar[0][0] = 1;	
+
+
+	float target = 40.0f;
 	float dt = 0.01;
 	float state = 0;
 	for(float t = 0; t < 1; t += dt){
-		float m = t;
-		assert(!kfPredict(&filter, NULL));
-		assert(!kfUpdate(&filter, &state, &m));
+		float m = target;
+		state = 0;
+		assert(!kfPredict(&f, NULL));
+		assert(!kfUpdate(&f, &state, &m));
+
+		Log("X_hat = %f", 1, state);
 	}
 
 	return 0;
@@ -21,6 +34,6 @@ int test(void)
 
 TEST_BEGIN
 	.name = "Filter Test",
-	.description = "Run the filter through a simple use case.",
+	.description = "Run the f through a simple use case.",
 	.run = test,
 TEST_END
